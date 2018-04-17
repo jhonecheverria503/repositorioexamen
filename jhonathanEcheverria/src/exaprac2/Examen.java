1a9f287aaa5101ce5b8bc47fcfc0bee6b4dfe786
Administrador <Administrador@172.16.17.107>
2018-04-17 14:18:11 -0600

    esquema de menu

--- jhonathanEcheverria/src/exaprac2/Examen.java
+++ jhonathanEcheverria/src/exaprac2/Examen.java
@@ -5,6 +5,8 @@
  */
 package exaprac2;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Administrador
@@ -12,7 +14,23 @@ package exaprac2;
 public class Examen {
     
     public static void main(String[] args) {
-        
+       
+        int r=0;
+       
+        while (r!=3) {
+              r= Integer.parseInt(JOptionPane.showInputDialog("Ingrese una opcion: \n\n 1)Agregar Producto \n 2)Calcular Todo \n 3)Salir "));
+        switch (r) {
+            case 1:
+                
+                
+                break;
+             
+            case 2:
+                
+                break;
+            
+        }   
+        }    
     }
     
 }
